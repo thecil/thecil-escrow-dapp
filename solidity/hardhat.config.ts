d5587585504030a85ce1b3a128310ede663f69ea
thecil <thecilsurf@gmail.com>
2024-09-09 15:05:07 -0400

    ref: sepolia deployment

--- solidity/hardhat.config.ts
+++ solidity/hardhat.config.ts
@@ -16,11 +16,11 @@ const config: HardhatUserConfig = {
         url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_SEPOLIA_KEY}`,
       }
     },
-    // sepolia: {
-    //   url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_SEPOLIA_KEY}`,
-    //   accounts:[process.env.SEPOLIA_PRIV_KEY as string],
-    //   chainId: 11155111,
-    // },
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_SEPOLIA_KEY}`,
+      accounts:[process.env.SEPOLIA_PRIV_KEY as string],
+      chainId: 11155111,
+    },
   },
   solidity: {
     compilers: [
@@ -51,7 +51,12 @@ const config: HardhatUserConfig = {
     path: './docs',
     clear: true,
     runOnCompile: true,
-  }
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: `${process.env.ETHERSCAN_API_KEY}`,
+    },
+  },
 };
 
 export default config;
