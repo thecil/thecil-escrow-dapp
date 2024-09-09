afc91908ded32b5b31e1e2f115cfbcd4db438ee8
thecil <thecilsurf@gmail.com>
2024-09-09 13:40:31 -0400

    feat: contract docgen

--- solidity/hardhat.config.ts
+++ solidity/hardhat.config.ts
@@ -2,6 +2,7 @@ import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
 import "hardhat-gas-reporter";
 import "hardhat-contract-sizer";
+import "hardhat-docgen";
 import * as dotenv from "dotenv";
 dotenv.config();
 
@@ -46,6 +47,11 @@ const config: HardhatUserConfig = {
   gasReporter: {
     L1: "ethereum",
   },
+  docgen: {
+    path: './docs',
+    clear: true,
+    runOnCompile: true,
+  }
 };
 
 export default config;
