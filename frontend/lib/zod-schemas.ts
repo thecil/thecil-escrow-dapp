491991b030e273864d40a525c8f295c55286da3b
thecil <thecilsurf@gmail.com>
2024-09-12 16:57:10 -0400

    Update zod-schemas.ts

--- frontend/lib/zod-schemas.ts
+++ frontend/lib/zod-schemas.ts
@@ -1,4 +1,5 @@
 import { z } from "zod";
+import { toUnixTime } from "./unix-time";
 
 export const tokenAmountSchema = z.bigint().min(1n);
 export const EvmAddressSchema = z.string().refine(
@@ -6,7 +7,7 @@ export const EvmAddressSchema = z.string().refine(
     // address = 42 chars
     value.startsWith("0x") && value.length === 42,
   {
-    message: "Ethereum address should be of length 42."
+    message: "Ethereum address should be of length 42.",
   }
 );
 
@@ -14,7 +15,9 @@ export const newEscrowSchema = z.object({
   beneficiary: EvmAddressSchema,
   tokenAddress: EvmAddressSchema,
   tokenAmount: tokenAmountSchema,
-  unlockTime: z.bigint().min(0n)
+  unlockTime: z
+    .date({ required_error: "Unlock time is required" })
+    .refine((value) => toUnixTime(value)),
 });
 
 export type NewEscrowSchemaFormInputs = z.infer<typeof newEscrowSchema>;
