775982cc714a39f5ecc187891b8d9f519197c26d
thecil <thecilsurf@gmail.com>
2024-09-12 18:10:54 -0400

    feat: ui comps & zod schemas

--- frontend/lib/zod-schemas.ts
+++ frontend/lib/zod-schemas.ts
@@ -1,13 +1,20 @@
 import { z } from "zod";
 import { toUnixTime } from "./unix-time";
 
-export const tokenAmountSchema = z.bigint().min(1n);
+export const tokenAmountSchema = z
+  .string()
+  .min(1, { message: "Enter a token amount" })
+  .regex(/^\d+(\.\d+)?$/);
+// .refine((value) => parseFloat(value) <= parseFloat(balanceOf), {
+//   message: "Chosen amount exceeds your balance"
+// })
+
 export const EvmAddressSchema = z.string().refine(
   (value) =>
     // address = 42 chars
     value.startsWith("0x") && value.length === 42,
   {
-    message: "Ethereum address should be of length 42.",
+    message: "Ethereum address should be of length 42."
   }
 );
 
@@ -17,7 +24,11 @@ export const newEscrowSchema = z.object({
   tokenAmount: tokenAmountSchema,
   unlockTime: z
     .date({ required_error: "Unlock time is required" })
-    .refine((value) => toUnixTime(value)),
+    .refine(
+      (date) => date > new Date(),
+      "Date must be higher than actual time."
+    )
+    .transform((date) => toUnixTime(date))
 });
 
 export type NewEscrowSchemaFormInputs = z.infer<typeof newEscrowSchema>;
