d80f3dfb6214abacba697f98d898e2762aa220fb
thecil <thecilsurf@gmail.com>
2024-09-18 15:10:10 -0400

    ref(escrow-tx-table): loading, error stages & show only user txs

--- frontend/components/web3/escrow-tx-table/index.tsx
+++ frontend/components/web3/escrow-tx-table/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useMemo } from "react";
 import { useReadEscrow } from "@/hooks/web3/contracts/use-read-escrow";
 
 import { columns } from "./columns";
@@ -6,28 +6,53 @@ import { DataTable } from "./data-table";
 
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { EscrowTx } from "@/types/escrow";
+import { useAccount } from "wagmi";
+import { Button } from "@/components/ui/button";
 
 const EscrowTxTable = () => {
   const {
     getAllEscrowsTx,
-    // refetchGetAllEscrowsTx,
+    refetchGetAllEscrowsTx,
     isLoadingGetAllEscrowsTx,
     getAllEscrowsTxError
   } = useReadEscrow();
-
+  const { address } = useAccount();
   useEffect(() => {
     console.log("getAllEscrowsTx", { getAllEscrowsTx });
   }, [getAllEscrowsTx]);
+
+  const _userEscrows = useMemo(() => {
+    if (!getAllEscrowsTx) return undefined;
+    const _escrows = getAllEscrowsTx.filter(
+      (escrowTx) => escrowTx.initiator === address
+    );
+    return _escrows;
+  }, [getAllEscrowsTx, address]);
   return (
     <Card>
       <CardHeader>
         <CardTitle>Step 3 - Manage Your Escrow Transactions</CardTitle>
       </CardHeader>
       <CardContent>
-        {isLoadingGetAllEscrowsTx && <div>isLoadingGetAllEscrowsTx</div>}
-        {getAllEscrowsTxError && <div>getAllEscrowsTxError</div>}
-        {getAllEscrowsTx && (
-          <DataTable columns={columns} data={getAllEscrowsTx as EscrowTx[]} />
+        {getAllEscrowsTxError && (
+          <div className="grid gap-4 justify-items-center">
+            <p className="text-2xl">
+              Something went wrong fetching the escrow transactions
+            </p>
+            <Button onMouseDown={() => refetchGetAllEscrowsTx()}>Retry</Button>
+          </div>
+        )}
+        {isLoadingGetAllEscrowsTx && (
+          <div className="animate-pulse flex w-full">
+            <div className="flex-1 space-y-2 py-1">
+              <div className="h-12 bg-slate-700 rounded"></div>
+              <div className="h-12 bg-slate-700 rounded"></div>
+
+            </div>
+          </div>
+        )}
+        {_userEscrows && (
+          <DataTable columns={columns} data={_userEscrows as EscrowTx[]} />
         )}
       </CardContent>
     </Card>
