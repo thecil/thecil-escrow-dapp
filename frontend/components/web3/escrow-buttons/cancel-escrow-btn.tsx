58ad4484d345f6614c85b54ce6044e529ed61e02
thecil <thecilsurf@gmail.com>
2024-09-19 15:51:03 -0400

    feat: escrow action buttons

--- frontend/components/web3/escrow-buttons/cancel-escrow-btn.tsx
+++ frontend/components/web3/escrow-buttons/cancel-escrow-btn.tsx
@@ -1,15 +1,99 @@
 import { Button } from "@/components/ui/button";
 import { EscrowTx, EscrowStatusEnum } from "@/types/escrow";
-import React from "react";
-import { useAccount } from "wagmi";
+import {
+  escrowContractInfo,
+  useReadEscrow
+} from "@/hooks/web3/contracts/use-read-escrow";
+import { FileSignature, Loader2 } from "lucide-react";
+import React, { useEffect } from "react";
+import { toast } from "sonner";
+import {
+  BaseError,
+  useAccount,
+  useWaitForTransactionReceipt,
+  useWriteContract
+} from "wagmi";
 
 const CancelEscrowButton = ({ escrowTx }: { escrowTx: EscrowTx }) => {
   const { address } = useAccount();
+  const { refetchGetAllEscrowsTx } = useReadEscrow();
+
   const _isInitiator = Boolean(address && address === escrowTx.initiator);
+  const {
+    data: cancelEscrowHash,
+    isPending: isPendingCancelEscrow,
+    writeContract: cancelEscrowTx
+  } = useWriteContract();
+
+  const {
+    isLoading: isConfirmingCancelEscrow,
+    isSuccess: isConfirmedCancelEscrow
+  } = useWaitForTransactionReceipt({
+    chainId: escrowContractInfo.chainId,
+    confirmations: 1,
+    hash: cancelEscrowHash,
+    query: {
+      enabled: Boolean(cancelEscrowHash)
+    }
+  });
+
+  const handleCancel = () =>
+    cancelEscrowTx(
+      {
+        ...escrowContractInfo,
+        functionName: "cancelEscrowTransaction",
+        args: [BigInt(escrowTx.id)]
+      },
+      {
+        onSuccess: (data) => {
+          console.log("cancelEscrowTransaction:onSuccess:", { data });
+          toast.info("Cancel Escrow tx signed, waiting for confirmation");
+        },
+        onError: (error) => {
+          console.log("cancelEscrowTransaction:onError:", { error });
+          toast.error(
+            `Error on Cancel Escrow: ${
+              (error as BaseError).shortMessage || error.message
+            }`
+          );
+        }
+      }
+    );
+
+  // triggers transaction confirmations
+  useEffect(() => {
+    if (isConfirmedCancelEscrow) {
+      toast.success(
+        `Cancel Escrow Tx ID ${escrowTx.id} success, \n Hash:${cancelEscrowHash}`
+      );
+      refetchGetAllEscrowsTx();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isConfirmedCancelEscrow]);
   return (
     <>
       {escrowTx.status === EscrowStatusEnum.Created && _isInitiator && (
-        <Button variant="destructive">Cancel</Button>
+        <Button
+          variant={"destructive"}
+          disabled={isPendingCancelEscrow || isConfirmingCancelEscrow}
+          onMouseDown={handleCancel}
+        >
+          <div className="flex items-center space-x-1">
+            {isPendingCancelEscrow ? (
+              <FileSignature className="mr-2 h-4 w-4 animate-ping" />
+            ) : null}
+            {isConfirmingCancelEscrow ? (
+              <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+            ) : null}
+            <p>
+              {isPendingCancelEscrow ? "Signing Tx" : null}
+              {isConfirmingCancelEscrow ? "Canceling" : null}
+              {!isPendingCancelEscrow && !isConfirmingCancelEscrow
+                ? "Cancel"
+                : null}
+            </p>
+          </div>
+        </Button>
       )}
     </>
   );
--- frontend/components/web3/escrow-buttons/dispute-escrow-btn.tsx
+++ frontend/components/web3/escrow-buttons/dispute-escrow-btn.tsx
@@ -1,27 +1,109 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import {
+  escrowContractInfo,
+  useReadEscrow
+} from "@/hooks/web3/contracts/use-read-escrow";
 import { unixNow } from "@/lib/unix-time";
 import { EscrowStatusEnum, EscrowTx } from "@/types/escrow";
-import React from "react";
-import { useAccount } from "wagmi";
+import { FileSignature, Loader2 } from "lucide-react";
+import React, { useEffect } from "react";
+import { toast } from "sonner";
+import {
+  BaseError,
+  useAccount,
+  useWaitForTransactionReceipt,
+  useWriteContract
+} from "wagmi";
 
 const DisputeEscrowButton = ({ escrowTx }: { escrowTx: EscrowTx }) => {
   const { address } = useAccount();
+  const { refetchGetAllEscrowsTx } = useReadEscrow();
   const _isInitiator = Boolean(address && address === escrowTx.initiator);
-  const _isBeneficiary = Boolean(address && address === escrowTx.beneficiary);
-  const _isParticipant = Boolean(_isInitiator || _isBeneficiary);
   const _isUnlocked = Boolean(escrowTx.unlockTime < BigInt(unixNow()));
+
+  const {
+    data: disputeEscrowHash,
+    isPending: isPendingDisputeEscrow,
+    writeContract: disputeEscrowTx
+  } = useWriteContract();
+
+  const {
+    isLoading: isConfirmingDisputeEscrow,
+    isSuccess: isConfirmedDisputeEscrow
+  } = useWaitForTransactionReceipt({
+    chainId: escrowContractInfo.chainId,
+    confirmations: 1,
+    hash: disputeEscrowHash,
+    query: {
+      enabled: Boolean(disputeEscrowHash)
+    }
+  });
+
+  const handleApproval = () =>
+    disputeEscrowTx(
+      {
+        ...escrowContractInfo,
+        functionName: "initiateDispute",
+        args: [BigInt(escrowTx.id)]
+      },
+      {
+        onSuccess: (data) => {
+          console.log("initiateDispute:onSuccess:", { data });
+          toast.info("Dispute Escrow tx signed, waiting for confirmation");
+        },
+        onError: (error) => {
+          console.log("initiateDispute:onError:", { error });
+          toast.error(
+            `Error on Dispute Escrow: ${
+              (error as BaseError).shortMessage || error.message
+            }`
+          );
+        }
+      }
+    );
+
+  // triggers transaction confirmations
+  useEffect(() => {
+    if (isConfirmedDisputeEscrow) {
+      toast.success(
+        `Dispute Escrow Tx ID ${escrowTx.id} success, \n Hash:${disputeEscrowHash}`
+      );
+      refetchGetAllEscrowsTx();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isConfirmedDisputeEscrow]);
+
   return (
     <>
-      {escrowTx.status === EscrowStatusEnum.Created && _isParticipant && (
-        <Button variant={"secondary"}>Dispute</Button>
+      {escrowTx.status === EscrowStatusEnum.Created && _isInitiator && (
+        <Button
+          variant={"secondary"}
+          disabled={isPendingDisputeEscrow || isConfirmingDisputeEscrow}
+          onMouseDown={handleApproval}
+        >
+          <div className="flex items-center space-x-1">
+            {isPendingDisputeEscrow ? (
+              <FileSignature className="mr-2 h-4 w-4 animate-ping" />
+            ) : null}
+            {isConfirmingDisputeEscrow ? (
+              <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+            ) : null}
+            <p>
+              {isPendingDisputeEscrow ? "Signing Tx" : null}
+              {isConfirmingDisputeEscrow ? "Entering Dispute" : null}
+              {!isPendingDisputeEscrow && !isConfirmingDisputeEscrow
+                ? "Dispute"
+                : null}
+            </p>
+          </div>
+        </Button>
       )}
       {escrowTx.status === EscrowStatusEnum.Dispute &&
-        _isInitiator &&
-        _isUnlocked && (
+        _isInitiator && (
         <>
-          <Button variant={"secondary"}>Approve Dispute</Button>
+          {_isUnlocked && <Button variant={"secondary"}>Approve Dispute</Button>}
           <Button variant={"secondary"}>Cancel Dispute</Button>
         </>
       )}
